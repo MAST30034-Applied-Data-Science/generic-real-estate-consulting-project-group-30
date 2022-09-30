948ac9bea32aec42d720d64011418f2548f628ee
Andrew <andrewpratama22@gmail.com>
2022-09-30 15:00:30 +1000

    updated for 2011 and 2016 census data compatibility

--- scripts/download_census.py
+++ scripts/download_census.py
@@ -1,25 +1,42 @@
-import requests
-import zipfile
-from io import BytesIO
-import shutil
+# note: year needs to be 2021, 2016, or 2011, any other value'd be invalid since the census doesn't exist every year
+def download_census(year):
+    import requests
+    import zipfile
+    from io import BytesIO
+    import shutil
 
-URL  = "http://abs.gov.au/census/find-census-data/datapacks/download/2021_GCP_SA2_for_VIC_short-header.zip"
+    if year == 2011:
+        # 2011 had BCP, not GCP
+        URL  = f"http://abs.gov.au/census/find-census-data/datapacks/download/{year}_BCP_SA2_for_VIC_short-header.zip"
+    else:
+        URL  = f"http://abs.gov.au/census/find-census-data/datapacks/download/{year}_GCP_SA2_for_VIC_short-header.zip"
 
-output_dir = "../data/raw/"
+    output_dir = "../data/raw/"
 
-# download zip file
-print("starting download")
-req = requests.get(URL)
-filename = URL.split('/')[-1]
+    # download zip file
+    print("starting download")
+    req = requests.get(URL)
+    filename = URL.split('/')[-1]
 
-zipfile = zipfile.ZipFile(BytesIO(req.content))
-zipfile.extractall(f"{output_dir}{filename}")
-print("download complete")
+    zipfile = zipfile.ZipFile(BytesIO(req.content))
+    zipfile.extractall(f"{output_dir}{filename}")
+    print("download complete")
 
-# delete unneeccessary files
-# i still don't know how to spell "uncesecsary" properly, please forgive me
-file_dir = f"{output_dir}{filename}/2021 Census GCP Statistical Area 2 for VIC/"
-shutil.move(f"{file_dir}2021Census_G02_VIC_SA2.csv", f"{output_dir}2021Census_G02_VIC_SA2.csv")
-shutil.move(f"{file_dir}2021Census_G17A_VIC_SA2.csv", f"{output_dir}2021Census_G17A_VIC_SA2.csv")
-shutil.move(f"{file_dir}2021Census_G17B_VIC_SA2.csv", f"{output_dir}2021Census_G17B_VIC_SA2.csv")
-shutil.rmtree(f"{output_dir}{filename}")
+    # delete unneeccessary files
+    # i still don't know how to spell "uncesecsary" properly, please forgive me
+    if year == 2011:
+        file_dir = f"{output_dir}{filename}/{year} Census BCP Statistical Areas Level 2 for VIC/VIC/"
+        shutil.move(f"{file_dir}{year}Census_B02_VIC_SA2_short.csv", f"{output_dir}{year}Census_B02_VIC_SA2_short.csv")
+        shutil.move(f"{file_dir}{year}Census_B17A_VIC_SA2_short.csv", f"{output_dir}{year}Census_B17A_VIC_SA2_short.csv")
+        shutil.move(f"{file_dir}{year}Census_B17B_VIC_SA2_short.csv", f"{output_dir}{year}Census_B17B_VIC_SA2_short.csv")
+        shutil.rmtree(f"{output_dir}{filename}")
+    else:
+        file_dir = f"{output_dir}{filename}/{year} Census GCP Statistical Area 2 for VIC/"
+        shutil.move(f"{file_dir}{year}Census_G02_VIC_SA2.csv", f"{output_dir}{year}Census_G02_VIC_SA2.csv")
+        shutil.move(f"{file_dir}{year}Census_G17A_VIC_SA2.csv", f"{output_dir}{year}Census_G17A_VIC_SA2.csv")
+        shutil.move(f"{file_dir}{year}Census_G17B_VIC_SA2.csv", f"{output_dir}{year}Census_G17B_VIC_SA2.csv")
+        shutil.rmtree(f"{output_dir}{filename}")
+
+download_census(2011)
+download_census(2016)
+download_census(2021)
